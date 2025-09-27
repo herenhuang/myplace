e2e77339c49e68caaf3a1ea488b7d670c68be31d
Heewon Ahn <shelost.off@gmail.com>
2025-09-26 21:44:38 -0400

    OAuth only

--- src/components/ui/UserButton.tsx
+++ src/components/ui/UserButton.tsx
@@ -1,7 +1,9 @@
 'use client';
 
 import Link from 'next/link';
+import Image from 'next/image';
 import type { User } from '@supabase/supabase-js';
+import { oAuthSignIn } from '../../app/auth/actions';
 
 interface UserButtonProps {
 	user: User | null;
@@ -10,25 +12,26 @@ interface UserButtonProps {
 const UserButton: React.FC<UserButtonProps> = ({ user }) => {
 	if (!user) {
 		return (
-			<div className="mt-8 z-10">
-				<Link
-					href="/signup"
-					className="px-5 py-3 bg-orange-500 hover:bg-orange-600 text-white text-base font-bold tracking-tight rounded-full shadow-md transition-colors duration-200"
-				>
+			<form action={oAuthSignIn}>
+				<input type="hidden" name="provider" value="google" />
+				<button type="submit" className="px-5 py-2 cursor-pointer bg-orange-500 hover:bg-orange-600 text-white text-base font-bold tracking-tight rounded-full shadow-md transition-colors duration-200">
 					Sign In
-				</Link>
-			</div>
+				</button>
+			</form>
 		);
 	}
 
 	return (
-		<Link href="/profile" className="mt-8 z-10 block">
-			<div className="bg-white/30 backdrop-blur-lg p-3 rounded-full flex items-center space-x-4 shadow-lg hover:bg-white/40 transition-all duration-200 cursor-pointer">
-				<div>
-					<p className="font-semibold tracking-tight text-black">{user.user_metadata.full_name}</p>
-					<p className="font-medium tracking-tight text-sm text-gray-700">{user.email}</p>
+		<Link href="/profile" className="z-10 block">
+			<div className="bg-white border border-gray-200 hover:border-orange-300 backdrop-blur-lg px-3 py-2.5 rounded-full flex items-center space-x-4 shadow-lg transition-all duration-200 cursor-pointer">
+				<div className="flex items-center space-x-2">
+					<Image src={user.user_metadata.avatar_url} alt="Profile" width={36} height={36} className="rounded-full" />
+					<div>
+						<p className="font-semibold tracking-tight leading-5 text-sm text-black">{user.user_metadata.full_name}</p>
+						<p className="font-medium tracking-tight leading-4 text-sm text-gray-400">{user.email}</p>
+					</div>
 				</div>
-				<div className="px-4 py-2 bg-black/10 text-black font-semibold rounded-full">View Profile</div>
+				<div className="px-4 py-2 hidden bg-black/10 hover:bg-black/20 transition-all duration-200 text-black tracking-tight text-sm font-semibold rounded-full">View Profile</div>
 			</div>
 		</Link>
 	);
