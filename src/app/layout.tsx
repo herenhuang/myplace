e3a2552ab311bfb896ca32add2e00e8287862b5b
Heewon Ahn <shelost.off@gmail.com>
2025-10-30 02:50:40 -0400

    Segmented Results

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -56,6 +56,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link 
+          rel="stylesheet" 
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" 
+        />
+      </head>
       <body
         className={`${inter.variable} ${instrumentSerif.variable} ${lora.variable} ${newsreader.variable} antialiased`}
         suppressHydrationWarning
